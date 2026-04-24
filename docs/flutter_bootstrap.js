061544fe557cea2abad0e0fba141c0209d3ec467
Sergey Burish <>
2026-04-24 10:45:53 +0300

    web build in docs update

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4146758524"
+    serviceWorkerVersion: "3276282046"
   }
 });
